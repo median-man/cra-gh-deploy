4468a900fb1faa9ad330e8d7d569d4a1757a2736
John Desrosiers <john.desro@gmail.com>
2019-04-23 10:37:28 -0700

    Updates

---
+++ precache-manifest.6763b2d962b23e00506982732b3d439d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fed3ec4c469b78404274577943e733",
+    "url": "/cra-gh-deploy/index.html"
+  },
+  {
+    "revision": "57b5c76355e8ca78f05f",
+    "url": "/cra-gh-deploy/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cra-gh-deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "57b5c76355e8ca78f05f",
+    "url": "/cra-gh-deploy/static/js/main.1507efb0.chunk.js"
+  },
+  {
+    "revision": "47f7574aa6f3fe30a7e4",
+    "url": "/cra-gh-deploy/static/js/runtime~main.03cf34ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-gh-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
